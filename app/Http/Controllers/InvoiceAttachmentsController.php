d61bb791c8a91c813837c3123429c12513b33c28
MostafaRhazlani <mo.rhazlani@gmail.com>
2023-09-07 23:29:39 +0100

    add db model controller for invoice attachments

---
+++ app/Http/Controllers/InvoiceAttachmentsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Invoice_attachments;
+use Illuminate\Http\Request;
+
+class InvoiceAttachmentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Invoice_attachments $invoice_attachments)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Invoice_attachments $invoice_attachments)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Invoice_attachments $invoice_attachments)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Invoice_attachments $invoice_attachments)
+    {
+        //
+    }
+}
